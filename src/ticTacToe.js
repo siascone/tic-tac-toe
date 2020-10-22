e97b880089fa4ade993ed516103c817b19652271
siascone <spencer.iascone@gmail.com>
2020-10-22 17:12:29 -0600

    base board logic

--- src/ticTacToe.js
+++ src/ticTacToe.js
@@ -1,7 +1,3 @@
-export class Game {
-
-}
-
 export class Board {
     constructor(gridSize) {
         this.gridSize = gridSize;
@@ -19,8 +15,36 @@ export class Board {
             }
         }
     }
-}
 
-export class Player {
+    isValidPosition(pos) {
+        return (
+            (pos[0] >= 0 && pos[0] < this.gridSize) &&
+            (pos[1] >= 0 && pos[1] < this.gridSize)
+        )
+    }
+
+    isPositionEmpty(pos) {
+        let x = pos[0];
+        let y = pos[1];
+
+        if (this.grid[x][y] === "_") return true
+
+        return false
+    }
+
+    placeMark(pos, mark) {
+        let x = pos[0];
+        let y = pos[1];
+        if (this.isValidPosition(pos) && this.isPositionEmpty(pos)) {
+            this.grid[x][y] = mark;
+            return true
+        } else {
+            return "Invalid move."
+        }
+    }
+
+    winRow(mark) {
+        
+    }
 
 }
